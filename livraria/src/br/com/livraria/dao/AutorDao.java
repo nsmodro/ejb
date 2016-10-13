bb74c3cd131f294869da84d0b3e5beccc8ad38bf
nsmodro <natacha.modro@gmail.com>
2016-10-12 22:53:04 -0300

    ok

--- livraria/src/br/com/livraria/dao/AutorDao.java
+++ livraria/src/br/com/livraria/dao/AutorDao.java
@@ -11,7 +11,21 @@ public class AutorDao {
 
 	private Banco banco = new Banco();
 
+	void aposCriacao() {
+		System.out.println("AutorDAO foi criado");
+	}
+	
 	public void salva(Autor autor) {
+		
+		System.out.println("Salvando autor" + autor.getNome());
+		try {
+			Thread.sleep(20000);//20s
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Salvou autor" + autor.getNome());
+		
 		banco.save(autor);
 	}
 	
